03a11bcefbb09ae5ef52b25500a97e8d7210fc74
rane254 <mr.appmaker.dev@gmail.com>
2025-04-06 21:12:55 +0530

    listening the events

--- my_bloc_console_app/main.dart
+++ my_bloc_console_app/main.dart
@@ -1,27 +1,29 @@
 import 'package:bloc/bloc.dart';
 
-void main() async {
-  
+Future<void> main(List<String> args) async {
   final cubit = CounterCubit();
+  final subscriber = cubit.stream.listen(print);
 
-  print(cubit.state);
-
-  cubit.counterInc();
-  print(cubit.state); // increases state + 1 = 1
+  // print(cubit.state);
 
   cubit.counterInc();
-  print(cubit.state); // increases state + 1 = 2
+  // print(cubit.state); // increases state + 1 = 1
 
   cubit.counterInc();
-  print(cubit.state); // increases state + 1 = 3
+  // print(cubit.state); // increases state + 1 = 2
+
+  cubit.counterInc();
+  // print(cubit.state); // increases state + 1 = 3
 
   cubit.counterDec();
-  print(cubit.state); // decreases state - 1 = 2
+  // print(cubit.state); // decreases state - 1 = 2
 
   cubit.counterDec();
-  print(cubit.state); // decreases state - 1 = 1
+  // print(cubit.state); // decreases state - 1 = 1
 
-  cubit.close();
+  await Future.delayed(Duration(seconds: 1));
+  await subscriber.cancel();
+  // cubit.close();
 }
 
 class CounterCubit extends Cubit<int> {
