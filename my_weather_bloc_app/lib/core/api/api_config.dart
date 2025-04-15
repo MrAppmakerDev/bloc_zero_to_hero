159e5e2092db19aaa7ae235e448d2515f456fadc
rane254 <mr.appmaker.dev@gmail.com>
2025-04-16 00:21:11 +0530

    updated config file

--- my_weather_bloc_app/lib/core/api/api_config.dart
+++ my_weather_bloc_app/lib/core/api/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
-  static const String apiKey = String.fromEnvironment('API_KEY',
-      defaultValue: '');
+  static const String apiKey =
+      String.fromEnvironment('API_KEY', defaultValue: '');
   static const String baseUrl = String.fromEnvironment('BASE_URL',
       defaultValue: 'http://api.weatherapi.com/v1/');
 }
